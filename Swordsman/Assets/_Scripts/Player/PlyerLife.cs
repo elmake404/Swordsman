2c153c70136ed51c63d2f5b0fd20b3634498ead0
Valentin <kibirevvalik202@gmail.com>
2021-02-08 15:21:36 +0300

    [update] Player

---
+++ Swordsman/Assets/_Scripts/Player/PlyerLife.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerLife : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var Enemy = other.GetComponent<Enemy>();
+
+        if (Enemy != null)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
