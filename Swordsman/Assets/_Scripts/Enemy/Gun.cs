06d9b197f37c2b7338b6c3e477522baaddc19f2c
Valentin <kibirevvalik202@gmail.com>
2021-02-09 13:13:51 +0300

    [add] Gan

---
+++ Swordsman/Assets/_Scripts/Enemy/Gun.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private Shell _shell;
+    [SerializeField]
+    private Transform _shootPos;
+
+    [SerializeField]
+    private float _timeShoot;
+
+    void Start()
+    {
+        StartCoroutine(Fire());   
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Sword")
+        {
+            Destroy(gameObject);
+
+            //gameObject.layer = 12;
+            //Vector3 direction = (transform.position - collision.GetContact(0).point).normalized;
+
+            //foreach (var halves in _sphereHalves)
+            //{
+            //    halves.gameObject.SetActive(true);
+            //    halves.Push(direction, _foresePushHalves);
+            //}
+
+            //Destruction();
+        }
+    }
+    //private void Destruction()
+    //{
+    //    PlayBlood();
+    //    Destroy(gameObject);
+    //}
+    //private void PlayBlood()
+    //{
+    //    if (_bloodPS != null)
+    //    {
+    //        _bloodPS.transform.SetParent(null);
+    //        _bloodPS.Play();
+    //        Destroy(_bloodPS, 1);
+    //    }
+    //}
+
+    private IEnumerator Fire()
+    {
+        if (_timeShoot<=0)
+        {
+            _timeShoot = 1;
+        }
+        while (true)
+        {
+            Instantiate(_shell,_shootPos.position,_shootPos.rotation);
+            yield return new WaitForSeconds(_timeShoot);
+        }
+    }
+}
---
+++ Swordsman/Assets/_Scripts/Enemy/Shell.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    [SerializeField]
+    private SphereHalves[] _sphereHalves;
+    [SerializeField]
+    private ParticleSystem _bloodPS;
+
+    [SerializeField]
+    private float _foresePushHalves,_speed,_timeLife;
+
+    private void Start()
+    {
+        Destroy(gameObject, _timeLife);
+        Destroy(gameObject, _timeLife);
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * _speed);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Sword" )
+        {
+            gameObject.layer = 12;
+            Vector3 direction = (transform.position - collision.GetContact(0).point).normalized;
+
+            foreach (var halves in _sphereHalves)
+            {
+                halves.gameObject.SetActive(true);
+                halves.Push(direction, _foresePushHalves);
+            }
+
+            Destruction();
+        }
+    }
+    private void Destruction()
+    {
+        PlayBlood();
+        Destroy(gameObject);
+    }
+    private void PlayBlood()
+    {
+        if (_bloodPS != null)
+        {
+            _bloodPS.transform.SetParent(null);
+            _bloodPS.Play();
+            Destroy(_bloodPS, 1);
+        }
+    }
+}
