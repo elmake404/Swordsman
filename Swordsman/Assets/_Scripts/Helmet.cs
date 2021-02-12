1adc18088e9c82d6d780584768b8196afc9fc49c
Valentin <kibirevvalik202@gmail.com>
2021-02-12 17:12:59 +0300

    [update] hlem

--- Swordsman/Assets/_Scripts/Helmet.cs
+++ Swordsman/Assets/_Scripts/Helmet.cs
@@ -7,9 +7,13 @@ public class Helmet : SphereHalves
     //[SerializeField]
     //private Collider _collider;
     [SerializeField]
+    private GameObject _helemNotRB, _helemRb;
+    [SerializeField]
     private float _forse;
     public void PushHelmet(Vector3 directonPush)
     {
+        _helemNotRB.SetActive(false);
+        _helemRb.SetActive(true);
         //_collider.enabled = true;
         _rbMain.isKinematic = false;
         Push(directonPush,_forse);
